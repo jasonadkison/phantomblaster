b7102447f88c601ab13fbe3376f2fdfe8cf7c728
Jason Adkison <jadkison@gmail.com>
2018-09-19 12:15:48 -0500

    specify response stub in spec

--- spec/phantomblaster/models/user_spec.rb
+++ spec/phantomblaster/models/user_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe Phantomblaster::Models::User do
 
   describe '.find' do
     context 'when successful' do
-      before(:each) { stub_user_get_request }
+      before(:each) do
+        stub_user_get_request(response_body: {
+          data: {
+            email: 'test@test.com',
+            agents: [{
+              name: 'Test Agent',
+              id: '12345',
+              scriptId: '54321',
+              lastEndStatus: '1234567890'
+            }]
+          }
+        }.to_json)
+      end
       subject { described_class.find }
 
       it 'should get the user email' do
@@ -14,10 +26,10 @@ RSpec.describe Phantomblaster::Models::User do
 
       it 'should get the user agents' do
         expect(subject.agents).to be_a(Array)
-        expect(subject.agents[0]['name']).to eq('Agent 1')
-        expect(subject.agents[0]['id']).to eq('1')
-        expect(subject.agents[0]['scriptId']).to eq('101')
-        expect(subject.agents[0]['lastEndStatus']).to eq('1537313102')
+        expect(subject.agents[0]['name']).to eq('Test Agent')
+        expect(subject.agents[0]['id']).to eq('12345')
+        expect(subject.agents[0]['scriptId']).to eq('54321')
+        expect(subject.agents[0]['lastEndStatus']).to eq('1234567890')
       end
     end
 
